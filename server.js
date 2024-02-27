c7ff72b7c6b1cf02902e37eeb11b5be703e245cc
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-02-27 11:44:32 -0600

    ejs snippets

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ var path = require('path');
 
 var app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.json());
 app.use(cors());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response){
-    response.render('index');
+    response.render('index', {
+        people: [
+            {name: 'dave'},
+            {name: 'jerry'}
+    ]    
+    });
 });
 
 app.listen(8000, function(){
